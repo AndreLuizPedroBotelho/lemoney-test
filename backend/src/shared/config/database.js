8bae0191082da0a304a04b4d17d4e2073e900c06
André <andre.gviva@gmail.com>
2021-01-17 00:02:24 -0300

    ajuste

--- backend/src/shared/config/database.js
+++ backend/src/shared/config/database.js
@@ -39,12 +39,10 @@ class Database {
   init() {
     this.connection = new Sequelize(this.connectionOption())
 
-    Promise.resolve(new Promise(async (resolve, reject) => {
-      models
-        .map(model => model.init(this.connection))
-        .map(model => model.associate && model.associate(this.connection.models))
-      resolve(true)
-    }))
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models))
+
 
     this.connection.sync()
 
